45ee0f4393550b74d55c26ac0ca4a1cef4a94631
Josh Reddick <josh.reddick@coxautoinc.com>
2019-01-15 19:31:53 -0500

    added serial ids for cams

--- src/bash/sortcams.sh
+++ src/bash/sortcams.sh
@@ -4,14 +4,15 @@
 # the cam serial number must be in the CAM_MAP.
 
 declare -A CAM_MAP
-CAM_MAP[7F11382F]=cam0
-CAM_MAP[ffff]=cam1
+CAM_MAP[046d_0821_4804CA90]=cam0  # logitech
+CAM_MAP[HD_Camera_Manufacturer_USB_2.0_Camera]=cam1  #fisheye.  no really.
 
 
 for cam in $(ls /dev/video*); do
         echo camera: $cam
         #sudo udevadm info --query=all /dev/video0 | grep 'VENDOR_ID\|MODEL_ID\|SERIAL_SHORT'
-        cam_info=$(sudo udevadm info --query=all $cam | grep 'SERIAL_SHORT' | cut -d'=' -f2)
+        #cam_info=$(sudo udevadm info --query=all $cam | grep 'SERIAL_SHORT' | cut -d'=' -f2)
+        cam_info=$(sudo udevadm info --query=all $cam | grep 'SERIAL=' | cut -d'=' -f2)
         echo $cam_info
         sudo ln -s $cam /dev/${CAM_MAP[$cam_info]}   
 done
